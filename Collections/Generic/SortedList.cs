f515dd21b3670f89b4c8a0e13ec9d18a7b8b83e6
Nikil2580 <cyanikil002@gmail.com>
2023-04-18 15:40:20 +0530

    Add comments

--- Collections/Generic/SortedList.cs
+++ Collections/Generic/SortedList.cs
@@ -4,16 +4,20 @@ public class SortedListExample
     {
         SortedList<string, int> customerDetails = new SortedList<string, int>();
 
+        // Add elements to list
         customerDetails.Add("Nikil", 20);
         customerDetails.Add("Kavin", 21);
         customerDetails.Add("Sujith", 19);
         customerDetails.Add("Vicky", 20);
         customerDetails.Add("Satha", 21);
 
+        //accessing elements using foreach
         foreach (var kvp in customerDetails)
         {
             Console.WriteLine("Customer Name : {0} \t Age: {1}", kvp.Key, kvp.Value);
         }
+
+        // Checking the capacity of SortedList
         Console.WriteLine("Capacity of SortedList: " + customerDetails.Capacity);
 
     }
